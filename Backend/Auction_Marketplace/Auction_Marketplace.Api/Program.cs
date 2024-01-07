19d8045b1d60d60bb1ae50477ac3113dbf73e2a0
Luben Kulishev <lyuben.kulishev@blankfactor.com>
2024-01-07 14:53:14 +0200

    Added empty auction_marketplace_services project, added auction_marketplace_data project with user entity and ApplicationDbContext, modify auction_marketplace_api project - implement the register method, added initial migration

--- Backend/Auction_Marketplace/Auction_Marketplace.Api/Program.cs
+++ Backend/Auction_Marketplace/Auction_Marketplace.Api/Program.cs
@@ -1,6 +1,30 @@
+using Auction_Marketplace_Data;
+using Auction_Marketplace_Data.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
