117a6f0fb4b07a2b3f8c892c7098026bb2159155
Savina <savina.valchanova@blankfactor.com>
2024-01-26 12:26:52 +0200

    causeviewmodel

---
+++ Backend/Auction_Marketplace/Auction_Marketplace.Data/Models/Stripe/CauseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Auction_Marketplace.Data.Models.Stripe
+{
+	public class CauseViewModel
+	{
+		public int Id { get; set; }
+
+		public string? Name { get; set; }
+
+		public decimal? Price { get; set; }
+	}
+}
+
